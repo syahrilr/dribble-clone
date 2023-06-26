8939c3aa9385acfeb15776422e2f8c2bec73f352
syahrilr <syahrilramadhan754@gmail.com>
2023-06-26 12:09:48 +0800

    update

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+'use client'
+
+import {getProviders, signIn} from 'next-auth/react'
+import { type } from 'os';
+import {useState, useEffect} from 'react'
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+}
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      console.log(res)
+
+      setProviders(res)
+    }
+
+    fetchProviders()
+  }, [])
+  
+  if(providers) {
+    return(
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+          <button onClick={() => signIn(provider?.id)} key={i}>{provider.id}</button>
+        ))}
+      </div>
+    )
+  }
+
+  return (
+    <div></div>
+  )
 };
 
 export default AuthProviders;
---
+++ lib/actions.ts
@@ -0,0 +1,14 @@
+import { GraphQLClient } from "graphql-request";
+
+const isProduction = process.env.NODE_ENV === 'production';
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || "" : 'http://'
+
+const client = new GraphQLClient('apiUrl')
+
+const makeGraphQLRequest = async (query: string, variables = {} ) => {
+    try {
+        
+    } catch (error) {
+        throw error
+    }
+}
